561e1270be20fab1b7e63088e6b8d710d1b4d748
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-10 17:04:43 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/stockmarketapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.metehanbolat.stockmarketapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.metehanbolat.stockmarketapp.ui.theme.StockMarketAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockMarketAppTheme {
+            }
+        }
+    }
+}
