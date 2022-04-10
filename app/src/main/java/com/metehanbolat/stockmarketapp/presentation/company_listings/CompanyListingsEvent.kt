73de5afdee0d5407da82abd1e13025a8354b1547
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-11 00:15:05 +0300

    CompanyListingsViewModel & CompanyListingsState & CompanyListingsEvent

---
+++ app/src/main/java/com/metehanbolat/stockmarketapp/presentation/company_listings/CompanyListingsEvent.kt
@@ -0,0 +1,6 @@
+package com.metehanbolat.stockmarketapp.presentation.company_listings
+
+sealed class CompanyListingsEvent {
+    object Refresh: CompanyListingsEvent()
+    data class OnSearchQueryChange(val query: String): CompanyListingsEvent()
+}
---
+++ app/src/main/java/com/metehanbolat/stockmarketapp/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.metehanbolat.stockmarketapp.presentation.company_listings
+
+import com.metehanbolat.stockmarketapp.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
