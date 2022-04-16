2f727656dd3c79cf0869c217ab16485cc04ae234
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-16 22:50:29 +0300

    CompanyInfoScreen and Navigate was completed.

--- app/src/main/java/com/metehanbolat/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/stockmarketapp/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.metehanbolat.stockmarketapp.presentation.company_listings.NavGraphs
+import com.metehanbolat.stockmarketapp.presentation.NavGraphs
 import com.metehanbolat.stockmarketapp.ui.theme.StockMarketAppTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/com/metehanbolat/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/metehanbolat/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.metehanbolat.stockmarketapp.presentation.destinations.CompanyInfoScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -54,7 +55,9 @@ fun CompanyListingsScreen(
                         modifier = Modifier
                             .fillMaxWidth()
                             .clickable {
-                                // TODO: Navigate to detail screen
+                                navigator.navigate(
+                                    CompanyInfoScreenDestination(company.symbol)
+                                )
                             }
                             .padding(16.dp)
                     )
